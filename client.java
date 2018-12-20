b9b0c837fd8bed4258d7ea48e89fa0d2f0529448
javier alejandro zavala torres <jazavala@cordillera.labs.inf.utfsm.cl>
2018-12-20 16:59:14 -0300

    >:3

--- client.java
+++ client.java
@@ -8,43 +8,5 @@ public class client {
 
     public client() {}
     //Conexión con servidor (Esto debe ser ejecutado por el proceso coordinador)
-    public static String Descifrar(String ruta_archivo, String ip_server) {
-
-		BufferedReader br = null;
-		FileReader fr = null;
-        String mensajeCifrado = "";
-		String numberGroup = "grupo_17";
-		try {
-
-			//br = new BufferedReader(new FileReader(FILENAME));
-			fr = new FileReader(ruta_archivo);
-			br = new BufferedReader(fr);
-			String sCurrentLine;
-
-			while ((sCurrentLine = br.readLine()) != null) {
-				mensajeCifrado += sCurrentLine;
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-				if (fr != null)
-					fr.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-        try {
-            Registry registry = LocateRegistry.getRegistry(ip_server);
-            InterfaceServer stub = (InterfaceServer) registry.lookup("PublicKey");
-            String uk = stub.getKey(numberGroup);
-            String mensajeDescifrado = stub.decipher(numberGroup, mensajeCifrado, uk);
-			return mensajeDescifrado;
-        } catch (Exception e) {
-            return "error";
-        }
-    }
+    
 }
